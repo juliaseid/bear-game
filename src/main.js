17e8151f6920acaae05d8fbf2276071020b00b80
Julia Seidman <juliaseid@gmail.com>
2020-04-21 16:30:07 -0700

    I don't think we made any more changes

--- src/main.js
+++ src/main.js
@@ -18,7 +18,21 @@ $(document).ready(function() {
   request.send();
 
   const getElements = function(response) {
-    $("#sleep").
+    $("#sleep").append("<a href= ")
   }
 
 });
+
+fetch('https://api.giphy.com/v1/gifs')
+.then(response => response.json())
+.then(data => {
+ console.log(data) // Prints result from `response.json()` in getRequest
+  Object.keys(data).forEach((key) => {
+    var ele = document.createElement("span");
+    var img = document.createElement("img");
+    img.setAttribute("src", VFZLRogThp41j9eepR[XR1V7LGX2hQhJ6qJH1QPZR1RFulM2Mc8]);
+    ele.appendChild(img);
+    //append ele to parent div
+  });
+})
+.catch(error => console.error(error))
